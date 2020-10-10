46b9d50c2c6fe25030bf1ffe3c6db7d3c0b0acb1
liyuan <1019343050@qq.com>
2020-10-10 17:20:00 +0800

    引入popup组件

--- compoents/tab-bar/tab-bar.js
+++ compoents/tab-bar/tab-bar.js
@@ -1,23 +1,35 @@
 // compoents/tab-bar/tab-bar.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {},
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onGoToHome: function () {
+            const log = 'onGoToHome';
+            this.triggerEvent('ongotohome',log);
+        },
+        onGoToCart: function () {
+            const log = 'onGoToCart';
+            this.triggerEvent('ongotocart', log);
+        },
+        onAddToCart: function () {
+            const log = 'onAddToCart';
+            this.triggerEvent('onaddtocart', log);
+        },
+        onBuy: function () {
+            const log = 'onBuy';
+            this.triggerEvent('onbuy',log)
+        }
+    }
+});
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
-})
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -3,16 +3,37 @@ import {SpuDetail} from "../../mode/spu-detail";
 Page({
 
     data: {
-        spuDetail:null
+        spuDetail: null,
+        showRealm: false
     },
-    onLoad:function (options) {
+    onLoad: function (options) {
         this.initSpuDetail(options.pid)
     },
-    async initSpuDetail(options){
+    async initSpuDetail(options) {
         const spuDetail = await SpuDetail.getSpuDetail(options);
         this.setData({
             spuDetail
         })
+    },
+    onGoToHome: function (event) {
+        console.log(event.detail)
+    },
+    onGoToCart: function (event) {
+
+        console.log(event.detail)
+    },
+    onAddToCart: function (event) {
+        this.setData({
+            showRealm:true
+        })
+        console.log(event.detail)
+    },
+    onBuy: function (event) {
+        this.setData({
+            showRealm:true
+        })
+        console.log(event.detail)
     }
+
 
 });
