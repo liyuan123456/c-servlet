9219d68e7c19b0ceb7f593178bbabb241dce06f8
liyuan <1019343050@qq.com>
2020-10-27 15:39:47 +0800

    search verson 1.1

--- mode/history-keyword.js
+++ mode/history-keyword.js
@@ -1,6 +1,16 @@
 class HistoryKeyWord{
-    static MAX_ITEM_COUNT = 20
-    keywords = []
+    static MAX_ITEM_COUNT = 20;
+    static KEY = 'KeyWords';
+    static instance
+    keywords = [];
+    constructor(){
+        if(typeof HistoryKeyWord.instance === 'object'){
+            return HistoryKeyWord.instance;
+        }
+        this.keywords = this._getLocalKeyWords();
+        HistoryKeyWord.instance = this;
+        return this;
+    }
     save(keyword){
         const item = this.keywords.filter(k => k === keyword);
         if(item.length != 0){
@@ -10,11 +20,28 @@ class HistoryKeyWord{
             this.keywords.pop()
         }
         this.keywords.unshift(keyword);
+        this._refreshLocal();
     }
     get(){
-        return this.keywords;
+        return this._getLocalKeyWords();
     }
     clear(){
         this.keywords = [];
+        this._refreshLocal();
     }
+    _refreshLocal(){
+        wx.setStorageSync(HistoryKeyWord.KEY,this.keywords);
+    }
+    _getLocalKeyWords(){
+        const keyWords = wx.getStorageSync(HistoryKeyWord.KEY);
+        if(keyWords){
+            return keyWords
+        }else{
+            wx.setStorageSync(HistoryKeyWord.KEY, []);
+            return [];
+        }
+    }
+}
+export {
+    HistoryKeyWord
 }
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,11 +1,33 @@
-// pages/search/search.js
+import {HistoryKeyWord} from "../../mode/history-keyword";
+
+const history = new HistoryKeyWord();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {},
-    onSearch(event){
-        const keyWord = event.detail.value;
+    onLoad(){
+        const historyTags = history.get();
+        this.setData({
+            historyTags
+        });
+    },
+    onSearch(event) {
+        const keyWord = event.detail.value.replace(/\s*/g,"");;
+        if(keyWord === ''){
+            return
+        }
+        console.log(keyWord);
+        history.save(keyWord);
+        this.setData({
+            historyTags: history.get()
+        });
+    },
+    onDeleteHistory(){
+        history.clear();
+        this.setData({
+            historyTags:[]
+        })
     }
-})
+});
